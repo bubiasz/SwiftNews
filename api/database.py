9553f3d6723d285c9e9c90545fc07f2e65ab2872
Bubiasz <jakub.matysek@outlook.com>
2023-11-08 20:30:30 +0100

    Configure PostgreSQL database connection

---
+++ api/database.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import config
+
+
+engine = sql.create_engine(config.DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
