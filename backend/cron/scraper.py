5b60f69bb44a64bcb8777727e13863ae9e6e6bcb
wbatek <wojciech.batko02@gmail.com>
2023-11-18 20:43:18 +0100

    scrapers implementation

--- backend/cron/scraper.py
+++ backend/cron/scraper.py
@@ -1,101 +1,33 @@
-import time
-import datetime as dt
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-from backend import config
-from backend.cron import schemas
+from backend.cron.scrapers import google_news_scraper, strategy_scraper
 
 
-class GoogleNewsScraper:
-    def __init__(self, language: str, location: str) -> None:
-        self.__options = None
-        self.__number_of_columns = len(config.CATEGORIES[(language, location)])
-        self.__leftmost = config.LEFTMOST_COLUMN_INDEX[(language, location)]
-        self.__language = language
-        self.__location = location
-        self.__articles = []
+class Scraper:
 
-        self.__configure_options()
-        self.__driver = webdriver.Chrome(options=self.__options)
-        self.__wait = WebDriverWait(self.__driver, 10)
-        self.__driver.get(
-            f"{config.NEWS_URL}hl={language}&gl={location.upper()}&ceid={location}:{language.upper()}")
+    def __init__(self, scraper: str, number_of_articles: int, language: str, location: str):
+        self.__scraper = ScraperFactory.create_scraper(scraper, number_of_articles, language, location)
+        self.__articles = list()
 
-        self.scrape_articles()
-        self.__driver.quit()
+    def set_language(self, language: str) -> None:
+        self.__scraper.set_language(language)
 
-    def __configure_options(self) -> None:
-        self.__options = Options()
-        self.__options.experimental_options['prefs'] = {
-            'profile.default_content_setting_values.notifications': 2,
-            'profile.default_content_setting_values.images': 2,
-            'profile.default_content_setting_values.autoplay': 2
-        }
+    def set_location(self, location: str) -> None:
+        self.__scraper.set_location(location)
 
-        self.__options.add_argument('--headless=new')
-        self.__options.add_argument('--disable-gpu')
+    def scraper_reset(self) -> None:
+        self.__articles = list()
+
+    def set_number_of_articles(self, number: int) -> None:
+        self.__scraper.set_number_of_articles(number)
 
     def scrape_articles(self) -> None:
-        buttons = self.__driver.find_elements(By.CSS_SELECTOR, config.ACCEPT_ALL_SELECTOR)
-        buttons[1].click()
-        try:
-            element = self.__wait.until(EC.presence_of_element_located((By.CLASS_NAME, config.HEADERS_SELECTOR)))
-        except TimeoutError:
-            print("Could not load page")
-            return
+        self.__scraper.scrape_articles()
 
-        # go through all headers
-        for current_idx in range(self.__leftmost, self.__leftmost + self.__number_of_columns):
-            header = self.__driver.find_elements(By.CLASS_NAME, config.HEADERS_SELECTOR)[current_idx]
-            self.__driver.get(header.get_attribute("href"))
+    def get_articles(self) -> list:
+        return self.__scraper.get_articles()
 
-            # get links to articles
-            article_links = self.__driver.find_elements(By.CLASS_NAME, config.ARTICLE_SELECTOR)[
-                            :config.NUMBER_OF_SCRAPED_ARTICLES_PER_CATEGORY]
 
-            # add columnId, name and url of article to articles list
-            for i in range(len(article_links)):
-                link = article_links[i].get_attribute("href")
-                final_url = self.get_final_url(link)
-                if final_url is None:
-                    continue
-
-                self.__articles.append((
-                    schemas.ParsedArticle(
-                        category=config.CATEGORIES[(self.__language, self.__location)][current_idx - self.__leftmost],
-                        language=self.__language,
-                        location=self.__location,
-                        url=final_url,
-                        date=dt.datetime.now(),
-                        title=None,
-                        content=None
-                    )))
-
-    def get_final_url(self, url: str) -> str:
-        driver = webdriver.Chrome(options=self.__options)
-        driver.get(url)
-        buttons = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button.VfPpkd-LgbsSe')))
-        buttons[1].click()
-
-        try:
-            time.sleep(1)
-            self.__wait.until(lambda x: x.execute_script("return document.readyState") == "complete")
-        except TimeoutError:
-            print("Could not load page")
-            return None
-
-        final_url = driver.current_url
-        driver.quit()
-        return final_url
-
-    def print_articles(self):
-        print(self.__articles)
-
-    def get_articles(self):
-        return self.__articles
+class ScraperFactory:
+    @staticmethod
+    def create_scraper(parser: str, number_of_articles: int, language: str, location: str):
+        if parser.lower() == "google":
+            return google_news_scraper.Scraper(number_of_articles, language, location)
